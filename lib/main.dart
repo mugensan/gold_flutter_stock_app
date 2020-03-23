41070c53f52862c2cdf3e6330ab3ceb95677f5eb
krodi001 <kev.roditi@gmail.com>
2020-03-23 10:07:48 +0100

    hardcoding dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:goldflutterstocks/widgets/HomePage.dart';
+import 'package:goldflutterstocks/pages/HomePage.dart';
 
 void main() => runApp(App());
 
---
+++ lib/widgets/stock_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class StockList extends StatelessWidget {
+
+  final List<Stock>stocks;
+
+  //  StockList structure
+  StockList({this.stocks});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
